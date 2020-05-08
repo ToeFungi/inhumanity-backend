c4b6c47c1d3add664e93dbe6573f3b09e4567738
Alex <6685713+ToeFungi@users.noreply.github.com>
2020-05-09 00:04:38 +0200

    feat(logging): Implement application logging with correlation ID

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import * as express from 'express'
+import * as HTTPContext from 'express-http-context'
+
+import { configuration } from './configuration'
+import { LoggerFactory } from './factories/logger-factory'
+import { CorrelationIdMiddleware } from './middleware/correlation-id-middleware'
 
 const server = express()
+  .use(HTTPContext.middleware)
+  .use(CorrelationIdMiddleware.getMiddleware())
 
-const port = process.env.PORT || 3000
+const loggerFactory = new LoggerFactory(configuration.logger)
+const logger = loggerFactory.getNamedLogger('application')
 
 const healthListener = (request: express.Request, response: express.Response) => {
+  logger.debug('health listener endpoint reached', { status: 200 })
   return response.status(200)
     .send('healthy')
 }
 
 server.get('/health', healthListener)
-  .listen(port, () => console.log('listening on port', { port }))
+
+server.listen(configuration.server.port, () => logger.info('Listening on specified port', {
+  port: configuration.server.port
+}))
